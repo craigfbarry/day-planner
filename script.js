34bc21435b4db5267d0dc5609e4d391bb6692fe1
craigfbarry <craigfbarry@gmail.com>
2020-03-20 20:50:48 +1030

    Added an array with 2 obkect types. Added logic to show 12hr clock with AM and PM

--- script.js
+++ script.js
@@ -3,11 +3,20 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
+var hourPresent = moment().hour();
+var schedule = [
+    {time:9, task:""},
+    {time:10, task:""},
+    {time:11, task:""},
+    {time:12, task:""},
+    {time:13, task:""},
+    {time:14, task:""},
+    {time:15, task:""},
+    {time:16, task:""},
+    {time:17, task:""},
+];
+
 for (i=0;i<9;i++){
-
-
-
-
 
 var hourBlock = $("<div>")
 var timeSlot = $("<div>")
@@ -17,10 +26,20 @@ var saveButton = $("<div>")
 //Code to dynamically add rows for each hour time slot and 3 columns for time, task, save button.
 hourBlock.addClass("row time-block");
 $("#workday").append(hourBlock);
-timeSlot.addClass("col-2 hour").text(i+9+":00 AM");
+timeSlot.addClass("col-2 hour");
+if(schedule[i].time>12){
+    timeSlot.text((schedule[i].time-12) +":00 PM");
+}
+else if (schedule[i].time===12){
+    timeSlot.text(schedule[i].time +":00 PM");
+}
+else{
+    timeSlot.text(schedule[i].time +":00 AM");
+}
 hourBlock.append(timeSlot);
-scheduleTask.addClass("col-8 future")
+
+scheduleTask.addClass("col-8 future");
 hourBlock.append(scheduleTask);
-saveButton.addClass("col-2 saveBtn")
+saveButton.addClass("col-2 saveBtn");
 hourBlock.append(saveButton);
 }
