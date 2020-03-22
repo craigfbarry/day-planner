ed727918b66467f8aaf992c8157e17d38c501d7c
craigfbarry <craigfbarry@gmail.com>
2020-03-22 21:04:05 +1030

    Added code to store multiple values in local storage on button click.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 
 //Added time and date with moment.Js will sort out formatting later
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
@@ -7,16 +8,19 @@ var hourPresent = moment().hour();
 var schedule = [
     {time:9, task:""},
     {time:10, task:""},
-    {time:11, task:""},
-    {time:12, task:""},
+    {time:11, task:"washing"},
+    {time:12, task:"Drying"},
     {time:13, task:""},
     {time:14, task:""},
     {time:15, task:""},
-    {time:22, task:""},
-    {time:23, task:""},
+    {time:16, task:""},
+    {time:17, task:""},
 ];
 
 for (i=0;i<9;i++){
+
+localStorage.getItem("savedTime");
+
 
 var hourBlock = $("<div>")
 var timeSlot = $("<div>")
@@ -38,7 +42,7 @@ else{
 }
 hourBlock.append(timeSlot);
 
-//Logic to compare the current hour from moment.js to the time array and apply the right class
+//Logic to compare the current hour from moment.js to the time array and apply the right class(past,future,present)
 
 if(schedule[i].time<hourPresent){
     scheduleTask.addClass("col-8 past");
@@ -50,7 +54,42 @@ else {
     scheduleTask.addClass("col-8 future");
 }
 
+
+//scheduleTask.attr("hour",schedule[i].time);
+//maybe remove one of these
+scheduleTask.attr("id","task-time-"+schedule[i].time);
+
+
+scheduleTask.text(schedule[i].task);
 hourBlock.append(scheduleTask);
+
+
+//Add data attributes and styling to save button, then append it to page.
+
 saveButton.addClass("col-2 saveBtn fas fa-calendar-alt pt-4");
+saveButton.attr("hour",schedule[i].time);
 hourBlock.append(saveButton);
 }
+
+
+$(".saveBtn").on("click", function(){
+    
+    let saveTime = ($(this).attr("hour"));
+    let saveTask = ($("#task-time-"+saveTime).val());
+    console.log(saveTime);
+    console.log(saveTask);
+    
+
+
+     
+
+    
+
+
+    localStorage.setItem("savedTime"+saveTime,saveTime);
+    localStorage.setItem("savedTask"+saveTime,saveTask);
+
+});
+
+
+});
